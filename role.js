a702f2e2cb499bfa95f8c99ff6595b2cd2c6b05c
Joy Johns <JRJ20716@gmail.com>
2023-02-20 21:25:09 -0500

    role and deparment

--- role.js
+++ role.js
@@ -1,8 +1,9 @@
 module.exports = class Employee {
-    constructor(id, first_name, last_name) {
+    constructor(id, first_name, last_name, role_id) {
         this.id = id;
         this.first_name = first_name;
         this.last_name = last_name;
+        this.role_id = role_id
         this.manager_id = null
     }
     getID(){
@@ -14,10 +15,15 @@ module.exports = class Employee {
     getLastName() {
         return this.last_name
     }
+    getRoleId() {
+        return this.role_id
+    }
     getManagerId() {
         return this.manager_id
     }
-
+    setRoleId(id) {
+        this.role_id = id
+    }
     setManagerId(id) {
         this.manager_id = id
     }
